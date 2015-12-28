d87ba1466304cfa1d34ccf588169b0d380b48caf
jxu093 <jxu093@gmail.com>
2015-12-27 19:56:28 -0500

    ok

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3:////home/dev/sq3db.db'
+
+
+class Url(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    url = db.Column(db.String, unique=False)
+
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+        
 
 @app.route('/')
 def index():
