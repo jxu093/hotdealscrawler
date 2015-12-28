33a434b8965e793aac09d055f3075cdd4e508c9e
jxu093 <jxu093@gmail.com>
2015-12-28 16:02:27 -0500

    db stuff

--- app.py
+++ app.py
@@ -9,14 +9,31 @@ db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/dev/sq3db.db'
 
 
-class Url(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True)
-    url = db.Column(db.String, unique=False)
+# class Url(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(50), unique=True)
+#     url = db.Column(db.String, unique=False)
+#
+#     def __init__(self, name, url):
+#         self.name = name
+#         self.url = url
 
-    def __init__(self, name, url):
-        self.name = name
+
+class Thread(db.Model):
+    __tablename__ = 'threads'
+
+    id = db.Column(db.String, primary_key=True)
+    title = db.Column(db.String)
+    views = db.Column(db.Integer)
+    url = db.Column(db.String)
+    lastpostdate = db.Column(db.String)
+
+    def __init__(self, id, title, views, url, lastpostdate):
+        self.id = id
+        self.title = title
+        self.views = views
         self.url = url
+        self.lastpostdate = lastpostdate
 
 
 @app.route('/')
